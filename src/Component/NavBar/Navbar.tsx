40967c498d0a2886bdf75736519c58b4a0672790
Eater228 <543145yura@gmail.com>
2025-01-14 15:32:24 +0200

    Fix bug with logout

--- src/Component/NavBar/Navbar.tsx
+++ src/Component/NavBar/Navbar.tsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
   // Обробник виходу з системи
   const handleLogout = () => {
     dispatch(logout());
-    navigate('/')
+    navigate('/Home')
   };
 
   const [showNotifications, setShowNotifications] = useState(false);
